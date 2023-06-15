cb09daf24bb2f357c3026d35696c12cd84f1f94f
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 19:18:13 +0300

    🐛 fix(main.rs): Disable raw mode before exiting The current implementation of the program leaves the terminal in raw mode when the program exits, which can cause issues for the user. To fix this, the `disable_raw_mode` function from the `termion` crate was added to disable raw mode before exiting the program. Changes made: - Added `terminal::disable_raw_mode()` function call before exiting the program. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,5 @@ fn main() {
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
+    terminal::disable_raw_mode().expect("Could not turn off raw mode");
 }
