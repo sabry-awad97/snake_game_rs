0df4ba565ad2f0415488910ecc6db072b621e29d
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 19:17:16 +0300

    🎉 feat(main.rs): Add raw mode to terminal 🐛 fix(main.rs): Fix error handling for enabling raw mode The `crossterm` crate was added to enable raw mode in the terminal. This allows for reading input without waiting for a newline character. To enable raw mode, the `terminal::enable_raw_mode()` function was called. However, error handling was not implemented for this function call. To fix this, a `match` expression was added to handle the `Err` case when enabling raw mode fails. Changes made: - The `crossterm` crate was added to enable raw mode in the terminal. - The `terminal::enable_raw_mode()` function was called to enable raw mode. - A `match` expression was added to handle the `Err` case when enabling raw mode fails. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
 use std::io::Read;
 
+use crossterm::terminal;
+
 fn main() {
+    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
 }
