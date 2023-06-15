02aa1f24a18b2d0afa2b31ccfe15cdd172521ac1
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 19:59:42 +0300

    🎉 feat(main.rs): Add Editor struct to handle keypress events This commit adds an `Editor` struct to handle keypress events. The `Reader` struct is used to read key events, and the `Editor` struct processes the key events and returns a boolean value indicating whether the loop should continue running or not. The `main` function now creates an instance of the `Editor` struct and runs the loop until the `process_keypress` function returns `false`. Changes made: - Added `Reader` struct to read key events. - Added `Editor` struct to process key events and return a boolean value indicating whether the loop should continue running or not. - Modified `main` function to create an instance of the `Editor` struct and run the loop until the `process_keypress` function returns `false`. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,50 @@ impl Drop for CleanUp {
     }
 }
 
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader }
+    }
+
+    fn process_keypress(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+                ..
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&self) -> crossterm::Result<bool> {
+        self.process_keypress()
+    }
+}
+
 fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?;
-    loop {
-        if event::poll(Duration::from_millis(500))? {
-            if let Event::Key(event) = event::read()? {
-                match event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: event::KeyModifiers::CONTROL,
-                        ..
-                    } => break,
-                    _ => {}
-                }
-                println!("{:?}\r", event);
-            };
-        } else {
-            println!("No input yet\r");
-        }
-    }
+    let editor = Editor::new();
+    while editor.run()? {}
     Ok(())
 }
