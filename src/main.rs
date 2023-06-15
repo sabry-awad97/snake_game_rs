dca12e251ba6b362d81c0956e8497349975b1f3d
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 19:25:22 +0300

    🎉 feat(main.rs): Add RAII cleanup for terminal raw mode This commit adds a struct `CleanUp` with a `Drop` implementation to handle the cleanup of the terminal raw mode. The `CleanUp` struct is instantiated at the beginning of the `main` function and is automatically dropped at the end of the function, ensuring that the terminal raw mode is always disabled, even in the case of a panic. Changes made: - A new struct `CleanUp` was added with a `Drop` implementation to handle the cleanup of the terminal raw mode. - An instance of `CleanUp` is created at the beginning of the `main` function to ensure that the terminal raw mode is always disabled. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,17 @@ use std::io::Read;
 
 use crossterm::terminal;
 
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode")
+    }
+}
+
 fn main() {
+    let _clean_up = CleanUp;
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    terminal::disable_raw_mode().expect("Could not turn off raw mode");
 }
