c913ac34f659778a9b6b68be381704a55722c4ca
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 19:49:20 +0300

    🎉 feat(main.rs): Add support for non-control characters and quit key 🐛 fix(main.rs): Improve error message for disabling raw mode The code has been modified to support non-control characters and a quit key. The `loop` construct has been used to continuously read input events from the terminal. The `match` expression has been used to handle different types of events, including the quit key and non-control characters. The error message for disabling raw mode has been improved to provide more context in case of an error. Changes made: - The `loop` construct has been used to continuously read input events from the terminal. - The `match` expression has been used to handle different types of events, including the quit key and non-control characters. - The error message for disabling raw mode has been improved to provide more context in case of an error. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
-use std::io;
-use std::io::Read;
-
-use crossterm::terminal;
+use crossterm::event::{Event, KeyCode, KeyEvent}; /* modify */
+use crossterm::{event, terminal};
 
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Could not disable raw mode")
+        terminal::disable_raw_mode().expect("Unable to disable raw mode")
     }
 }
 
 fn main() {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
-    let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
-        let character = buf[0] as char;
-        if character.is_control() {
-            println!("{}\r", character as u8)
-        } else {
-            println!("{}\r", character)
-        }
+    loop {
+        if let Event::Key(event) = event::read().expect("Failed to read line") {
+            match event {
+                KeyEvent {
+                    code: KeyCode::Char('q'),
+                    modifiers: event::KeyModifiers::NONE,
+                    ..
+                } => break,
+                KeyEvent {
+                    code: KeyCode::Char(c),
+                    modifiers: event::KeyModifiers::NONE,
+                    ..
+                } => {
+                    println!("{}", c);
+                }
+                _ => {}
+            }
+        };
     }
 }
