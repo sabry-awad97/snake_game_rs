bc0bcc7ba25c6596976caf2f31f26d1023cc379c
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-15 20:31:50 +0300

    🎉 feat(main.rs): Add snake movement and collision detection 🐛 fix(main.rs): Fix missing semicolon in draw_food function ✨ feat(main.rs): Add snake direction and food collision detection The code now includes snake movement and collision detection with walls, self, and food. The snake can be controlled using the arrow keys, and the food is randomly generated on the screen. The game loop now runs at a fixed rate, and the snake direction is updated based on user input. The `draw_food` function was also updated to include a missing semicolon. Changes made: - Added `Direction` enum to represent the snake's direction. - Added `update_snake` function to update the snake's position and check for collisions. - Updated `process_keypress` function to update the snake's direction based on user input. - Updated `run` function to call `update_snake` and return false if there is a collision. - Updated `main` function to run the game loop at a fixed rate and check for user input. - Added constants for screen width and height. - Added initial snake position, food position, and direction. - Updated `draw_food` function to include a missing semicolon. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ use crossterm::terminal::ClearType;
 use crossterm::{cursor, event, execute, terminal};
 use std::collections::LinkedList;
 use std::io::stdout;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+const WIDTH: u16 = 40;
+const HEIGHT: u16 = 20;
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 struct CleanUp;
 
@@ -63,7 +73,8 @@ impl Output {
     ) -> crossterm::Result<()> {
         Self::clear_screen()?;
         Self::draw_snake(snake)?;
-        Self::draw_food(food)
+        Self::draw_food(food)?;
+        Ok(())
     }
 }
 
@@ -72,34 +83,92 @@ struct Game {
     output: Output,
     snake: LinkedList<(u16, u16)>,
     food: (u16, u16),
+    direction: Direction,
 }
 
 impl Game {
     fn new() -> Self {
         let mut snake = LinkedList::new();
-        snake.push_back((2, 2));
+        snake.push_back((2, 2)); // Initial snake position
+
         Self {
             reader: Reader,
-            snake,
-            food: (20, 10),
             output: Output::new(),
+            snake,
+            food: (20, 10),              // Initial food position
+            direction: Direction::Right, // Initial snake direction
         }
     }
 
-    fn process_keypress(&self) -> crossterm::Result<bool> {
+    fn process_keypress(&mut self) -> crossterm::Result<bool> {
         match self.reader.read_key()? {
             KeyEvent {
                 code: KeyCode::Char('q'),
                 modifiers: event::KeyModifiers::CONTROL,
                 ..
             } => return Ok(false),
+            KeyEvent {
+                code: KeyCode::Up, ..
+            } => self.direction = Direction::Up,
+            KeyEvent {
+                code: KeyCode::Down,
+                ..
+            } => self.direction = Direction::Down,
+            KeyEvent {
+                code: KeyCode::Left,
+                ..
+            } => self.direction = Direction::Left,
+            KeyEvent {
+                code: KeyCode::Right,
+                ..
+            } => self.direction = Direction::Right,
             _ => {}
         }
         Ok(true)
     }
 
-    fn run(&self) -> crossterm::Result<bool> {
+    fn update_snake(&mut self) -> bool {
+        let (x, y) = self.snake.front().unwrap().clone();
+        let new_head = match self.direction {
+            Direction::Up => (x, y - 1),
+            Direction::Down => (x, y + 1),
+            Direction::Left => (x - 1, y),
+            Direction::Right => (x + 1, y),
+        };
+
+        // Check for collision with walls
+        if new_head.0 == 0 || new_head.0 >= WIDTH - 1 || new_head.1 == 0 || new_head.1 >= HEIGHT - 1
+        {
+            return false;
+        }
+
+        // Check for collision with self
+        if self.snake.contains(&new_head) {
+            return false;
+        }
+
+        self.snake.push_front(new_head);
+
+        // Check for collision with food
+        if new_head == self.food {
+            self.food = (
+                1 + rand::random::<u16>() % (WIDTH - 2),
+                1 + rand::random::<u16>() % (HEIGHT - 2),
+            );
+        } else {
+            self.snake.pop_back();
+        }
+
+        true
+    }
+
+    fn run(&mut self) -> crossterm::Result<bool> {
         self.output.refresh_screen(&self.snake, &self.food)?;
+
+        if !self.update_snake() {
+            return Ok(false);
+        }
+
         self.process_keypress()
     }
 }
@@ -107,7 +176,29 @@ impl Game {
 fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?;
-    let game = Game::new();
-    while game.run()? {}
+    let mut game = Game::new();
+
+    let mut last_update = Instant::now();
+    let update_rate = Duration::from_millis(100);
+
+    loop {
+        if event::poll(Duration::from_millis(1))? {
+            if let Event::Key(event) = event::read()? {
+                if event.code == KeyCode::Char('q')
+                    && event.modifiers == event::KeyModifiers::CONTROL
+                {
+                    break;
+                }
+            }
+        }
+
+        if Instant::now() - last_update >= update_rate {
+            if !game.run()? {
+                break;
+            }
+            last_update = Instant::now();
+        }
+    }
+
     Ok(())
 }
